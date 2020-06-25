42d38c9b975e616e2d98de1c8ad4c9aa217d8fef
Yeong Yong Sheng <66880085+Yeong19@users.noreply.github.com>
2020-06-25 11:22:02 +0800

    P2Q1

---
+++ src/p2q1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Yeong Yong Sheng
+ */
+public class p2q1 {
+
+  public static void displaySquareRoot(int max){
+     System.out.println("Number  SquareRoot");
+
+     for(int i=0; i<=max; i+=2){
+        System.out.printf("%d \t %.4f\n", i, Math.sqrt(i));
+     }
+  }
+
+  public static void main(String[] args) {
+     displaySquareRoot(20);
+}
+}
